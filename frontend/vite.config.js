440e3010d7e1eafe05afeb537a98356bca621288
David-Nguyen <ThuanNgcodelor>
2026-07-03 16:31:06 +0700

    Update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true, // Mở cho mạng LAN
+  },
   define: {
     global: 'window',
   },
